d80bf104e0fb5e08819c94a36f20ba6395de3653
Shubham Aswal <shubhamaswal@Shubhams-MacBook-Air.local>
2025-04-18 23:17:22 +0530

    healthcheck added

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,13 @@
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import userRouter from "./user.router";
 import { paymentController } from "../controllers/payment/payment.controller";
 import paymentRouter from "./payment.router";
 
 export default function routes(app: Application): void {
+  // Healthcheck route
+  app.get("/api/v1/healthcheck", (req: Request, res: Response) => {
+    res.status(200).json({ status: "ok", message: "Server is healthy" });
+  });
   app.use("/api/v1/user", userRouter);
   app.use("/api/v1/payment", paymentRouter);
 }
